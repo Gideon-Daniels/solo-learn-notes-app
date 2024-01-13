2b463275d6e99dabb166018cf145dbf924b3b484
Gideon Daniels <gideon.daniels@safetyio.com>
2024-01-13 16:34:10 +0200

    solo-learn notes app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "./header/header.component";
+import {AddNoteComponent} from "./add-note/add-note.component";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['app.component.scss'],
+  imports: [HeaderComponent, AddNoteComponent],
 })
-export class AppComponent {
-  title = 'new-app';
-}
+export class AppComponent {}
